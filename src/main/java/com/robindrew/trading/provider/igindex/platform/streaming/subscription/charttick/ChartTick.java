b225d6f506cca47b3de539cb86135fbba84454d3
Robin Drew <robin.drew@gmail.com>
2018-04-03 12:42:28 +0100

    Refactored chart tick

--- src/main/java/com/robindrew/trading/provider/igindex/platform/streaming/subscription/charttick/ChartTick.java
+++ src/main/java/com/robindrew/trading/provider/igindex/platform/streaming/subscription/charttick/ChartTick.java
@@ -4,18 +4,22 @@ import java.math.BigDecimal;
 
 import com.robindrew.trading.IInstrument;
 import com.robindrew.trading.price.candle.IPriceCandle;
-import com.robindrew.trading.price.candle.PriceCandleInstant;
 import com.robindrew.trading.price.candle.format.pcf.FloatingPoint;
 import com.robindrew.trading.price.precision.IPricePrecision;
 import com.robindrew.trading.price.tick.PriceTick;
 
-public class ChartTick extends PriceTick {
+public class ChartTick {
 
 	private final IInstrument instrument;
 	private final IPricePrecision precision;
+	private final long timestamp;
+	private final BigDecimal bid;
+	private final BigDecimal ask;
 
 	public ChartTick(IInstrument instrument, IPricePrecision precision, String itemName, String timestamp, String bid, String ask) {
-		super(Long.parseLong(timestamp), new BigDecimal(bid), new BigDecimal(ask));
+		this.timestamp = Long.parseLong(timestamp);
+		this.bid = new BigDecimal(bid);
+		this.ask = new BigDecimal(ask);
 		this.instrument = instrument;
 		this.precision = precision;
 	}
@@ -24,13 +28,29 @@ public class ChartTick extends PriceTick {
 		return instrument;
 	}
 
+	public IPricePrecision getPrecision() {
+		return precision;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public BigDecimal getBid() {
+		return bid;
+	}
+
+	public BigDecimal getAsk() {
+		return ask;
+	}
+
 	public IPriceCandle toPriceCandle() {
 		BigDecimal bid = getBid();
 		BigDecimal ask = getAsk();
 		int decimalPlaces = precision.getDecimalPlaces();
 		int bidPrice = FloatingPoint.toBigInt(bid, decimalPlaces);
 		int askPrice = FloatingPoint.toBigInt(ask, decimalPlaces);
-		return new PriceCandleInstant(bidPrice, askPrice, getTimestamp(), decimalPlaces);
+		return new PriceTick(bidPrice, askPrice, getTimestamp(), decimalPlaces);
 	}
 
 }
